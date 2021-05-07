53bd0144352733c45311230dd1e810fdccfa19fb
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-05-08 01:39:43 +0900

    [improve] store speedtest result to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import {speedTest} from "./command/speed-test";
+import {MongoClient} from "mongodb";
+
+
+const INSERT_COMPLETED = 1;
 
 
 main()
 
 
 function main() {
-    speedTest()
-        .then(stdout => JSON.parse(stdout))
-        .then(result => {
-            console.log(result)
+    const mongoClient = MongoClient.connect('mongodb://192.168.3.192:9017', {useUnifiedTopology: true})
+
+    Promise.all([speedTest(), MongoClient.connect('mongodb://192.168.3.192:9017', {useUnifiedTopology: true})])
+        .then(([result, client]) => {
+            const resultObject = JSON.parse(result)
+
+            return client.db('speedtest').collection('results').insertOne(resultObject)
+        })
+        .then(insertResult => {
+            if (insertResult.result.ok != INSERT_COMPLETED) throw ''
+        })
+        .catch(err => {
+            console.error(err)
         })
         .finally(() => {
-            console.log('finished!')
+            mongoClient.then(client => {
+                client.close()
+                    .then()
+            })
         })
 }
