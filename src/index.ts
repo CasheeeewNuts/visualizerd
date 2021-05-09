0f5b3c0b03323a3a0edac57773fee51b866eab85
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-05-09 16:36:37 +0900

    [improve] add error handling

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,35 @@ main()
 
 
 function main() {
-    const mongoClient = MongoClient.connect('mongodb://192.168.3.192:9017', {useUnifiedTopology: true})
+    let mongoClient: MongoClient
+    let onError: boolean = false
 
-    Promise.all([speedTest(), MongoClient.connect('mongodb://192.168.3.192:9017', {useUnifiedTopology: true})])
-        .then(([result, client]) => {
-            const resultObject = JSON.parse(result)
+    Promise.all([
+        speedTest(),
+        MongoClient.connect('mongodb://192.168.3.192:8017', {useUnifiedTopology: true})
+    ]).then(([result, client]) => {
+        mongoClient = client
+        const resultObject = JSON.parse(result)
 
-            return client.db('speedtest').collection('results').insertOne(resultObject)
+        return client.db('speedtest').collection<string>('results').insertOne(resultObject)
+    }).then(insertResult => {
+        if (insertResult.result.ok != INSERT_COMPLETED) throw new Error('error')
+
+        console.log('test completed!')
+    }).catch((err: Error) => {
+        onError = true
+        console.error(err.message)
+    }).finally(() => {
+        if (mongoClient == null) {
+            process.exit(1)
+        }
+
+        mongoClient.close().finally(() => {
+            console.log('connection closed!')
+
+            if (onError) process.exit(1)
+
+            process.exit(0)
         })
-        .then(insertResult => {
-            if (insertResult.result.ok != INSERT_COMPLETED) throw ''
-        })
-        .catch(err => {
-            console.error(err)
-        })
-        .finally(() => {
-            mongoClient.then(client => {
-                client.close()
-                    .then()
-            })
-        })
+    })
 }
