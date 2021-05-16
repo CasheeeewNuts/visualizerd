0ecedfa5a4c892e767de9a2deade3de130aa14c1
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-05-16 17:33:32 +0900

    [improve] make be able to schedule process

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 
+require('dotenv/config')
 const cron = require('node-cron')
+const path = require('path')
 const {fork} = require('child_process')
 
-cron.schedule('*/1 * * * *', () => {
-  const childProcess = fork(`${__dirname}/thread.js`)
+const DEFAULT_SCHEDULE = '*/5 * * * *'
+const {SCHEDULE = DEFAULT_SCHEDULE} = process.env
+
+cron.schedule(SCHEDULE, () => {
+  const module = path.join(__dirname, 'thread.js')
+  const childProcess = fork(module)
 
   childProcess.send('execute')
 })
